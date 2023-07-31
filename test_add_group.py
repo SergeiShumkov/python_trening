19b8ff63ddb0b10eb6afbd9ac0ad7c91f4079c8f
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-07-31 08:07:22 +0500

    Незначительное улучшение кода из Katalon Recoder

--- test_add_group.py
+++ test_add_group.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,47 +8,62 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
+
+DRIVER_FOLDER = os.path.expanduser("~\Downloads\drivers")
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome(executable_path=f"{DRIVER_FOLDER}\chromedriver\chromedriver.exe",
+                         options=options
+                         )
+
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = driver
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_id("LoginForm").submit()
+
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("dfgdf")
+        wd.find_element_by_name("group_name").send_keys("new_group")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dfggdd")
+        wd.find_element_by_name("group_header").send_keys("header_group")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("dfdgdddff")
+        wd.find_element_by_name("group_footer").send_keys("footer_group")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-    
+
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
 
     def tearDown(self):
         self.wd.quit()
