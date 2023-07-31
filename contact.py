ac96f0466b8b8fd07a872d96636da9b70406738e
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-07-31 11:44:26 +0500

    Создан вспомогательный класс Contact

---
+++ contact.py
@@ -0,0 +1,11 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, company_name, home_phone_number):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company_name = company_name
+        self.home_phone_number = home_phone_number
+
