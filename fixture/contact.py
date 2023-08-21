6f9d0aea68e6fbfc2144eaeb893f6bc016d8a11f
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-08-21 09:57:56 +0500

    Добавили выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -69,8 +69,11 @@ class ContactHelper:
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
 
-
-
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,7 +1,11 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
 
 def test_modify_contact_home_phone_number(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(home_phone_number="11-11-11"))
