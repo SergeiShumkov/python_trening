b03d126aa1d4b789e441280554b67daaa48dc624
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-08-28 13:01:44 +0500

    Отделили тестовые данные от теста и добавили генерацию случайных тестовых данных

--- model/group.py
+++ model/group.py
@@ -1,7 +1,7 @@
 from sys import maxsize
 
-class Group:
 
+class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.header = header
@@ -9,10 +9,11 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return f"{self.id}:{self.name}"
+        return f"{self.id}:{self.name};{self.header};{self.footer}"
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id ) and self.name == other.name
+
     def id_or_max(self):
         if self.id:
             return int(self.id)
