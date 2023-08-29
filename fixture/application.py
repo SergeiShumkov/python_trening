84c6d618e703d430ffd3ccff8e136d9319800915
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-08-29 13:59:43 +0500

    Реализовали опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -12,14 +12,24 @@ options = webdriver.ChromeOptions()
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome(executable_path=f"{DRIVER_FOLDER}\chromedriver\chromedriver.exe",
-                         options=options
-                         )
+    def __init__(self, browser, base_url):
+
+        # self.wd = webdriver.Chrome(executable_path=f"{DRIVER_FOLDER}\chromedriver\chromedriver.exe",
+        #                  options=options
+        #                  )
+
+        if browser == "chrome":
+            self.wd = webdriver.Chrome(executable_path=f"{DRIVER_FOLDER}\chromedriver\chromedriver.exe")
+        elif browser == "opera":
+            self.wd = webdriver.Opera(executable_path=f"{DRIVER_FOLDER}\operadriver\operadriver.exe")
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
+
         # self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -31,7 +41,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("addressbook/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
