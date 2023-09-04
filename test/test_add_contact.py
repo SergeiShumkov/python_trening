4edec036c5986fb8c20d0e7ef83ea5799e7a8811
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-09-04 10:07:18 +0500

    Реализовали загрузку тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import re
-import pytest
 
 from model.contact import Contact
-from data.add_contact import constant as testdata
 
 
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     contact.all_emails_from_home_page = merge_emails([contact.email1, contact.email2, contact.email3])
     contact.all_phones_from_home_page = merge_phones([contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone])
     old_contacts = app.contact.get_contact_list()
@@ -17,6 +14,7 @@ def test_add_contact(app, contact):
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
 def clear(s):
     return re.sub("[() -]", "", s)
