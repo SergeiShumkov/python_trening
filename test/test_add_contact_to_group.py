abeca8dbc5cf513a201f005ff25c3136183a0be2
SergeiShumkov <sergeishumkov72@yahoo.com>
2023-09-14 09:18:39 +0500

    Добавили функционал: для теста берется контакт, который не добавлен в группу и добавляется этот контакт в такую группу, в которой он на момент добавления не состоит. Так же проводится проверка, что все контакты добавлены в о все группы. При этом условии случайный контакт удаляется из случайной группы

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,16 +8,23 @@ def test_add_contact_to_group(app, db, db_orm, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test"))
     contacts = db.get_contact_list()
-    contact = random.choice(contacts)
+    # contact = random.choice(contacts)
 
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     groups = db.get_group_list()
-    group = random.choice(groups)
+    # group = random.choice(groups)
 
+    arr = [(contact, group) for contact in contacts for group in groups if contact not in db_orm.get_contacts_in_group(group)]
+
+    if len(arr) == 0: # Проверка, что все контакты добавлены во все группы
+        contact_del = random.choice(contacts)
+        group_del = random.choice(groups)
+        app.contact.del_contact_from_group(contact_del.id, group_del.name)
+        contact = contact_del
+        group = group_del
+    else:
+        contact, group = random.choice(arr)
     app.contact.add_contact_to_group(contact.id, group.name)
     l = db_orm.get_contacts_in_group(group)
     assert contact in l
-
-
-
